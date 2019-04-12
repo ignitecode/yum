0f5278d60e37cdfc3145612f1444880736586a05
Christian <cbartram3@gmail.com>
2019-04-12 15:09:21 -0400

    Added update method

--- routes/controllers/restaurant_controller.js
+++ routes/controllers/restaurant_controller.js
@@ -11,7 +11,29 @@ router.get('/', (req, res) => {
 
 router.get('/create', (req, res) => {
     res.render('restaurants/create_restaurant');
-    // restaurant.create({ ...req.body, createdAt: new Date(), updatedAt: new Date() })
+});
+
+router.post('/create', (req, res) => {
+  restaurant.create({ ...req.body, rating: +req.body.rating, createdAt: new Date(), updatedAt: new Date() })
+    .then(restaurant => {
+      console.log('[INFO] Created Restaurant Successfully!');
+      res.redirect('/restaurants');
+    })
+});
+
+router.get('/update/:id', (req, res) => {
+  restaurant.findByPk(req.params.id).then(restaurant => {
+    res.render('restaurants/update_restaurant', { restaurant });
+  });
+});
+
+router.put('/update/:id', (req, res) => {
+  restaurant.findByPk(req.params.id).then(rest => {
+    rest.update({ ...req.body, createdAt: new Date(), updatedAt: new Date() })
+    .then(updatedRestaurant => {
+        res.redirect('/restaurants');
+    });
+  });
 });
 
 module.exports = router;
